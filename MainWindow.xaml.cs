d4315452ec1c287f832c0aabe8b44d7a96ee5522
tchait0903 <115585994+tchait0903@users.noreply.github.com>
2023-04-06 18:04:22 -0400

    init wpf 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace COFBotDetection_1
+namespace COFBotDetection
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- obj/Debug/net7.0-windows/COFBotDetection.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/COFBotDetection.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("COFBotDetection-1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("COFBotDetection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("COFBotDetection-1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("COFBotDetection-1")]
+[assembly: System.Reflection.AssemblyProductAttribute("COFBotDetection")]
+[assembly: System.Reflection.AssemblyTitleAttribute("COFBotDetection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
