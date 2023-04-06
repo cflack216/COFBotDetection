e13a50a1d8c914c2cb4660a7e2cf813498cc9393
cflack216 <2775446@vikes.csuohio.edu>
2023-04-06 18:32:22 -0400

    yes

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace COFBotDetection
+{
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == "admin" && password == "password")
+            {
+                MessageBox.Show("Login successful.");
+            }
+            else
+            {
+                ErrorTextBlock.Text = "Incorrect username or password.";
+            }
+        }
+    }
+}
